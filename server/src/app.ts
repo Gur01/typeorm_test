a77d7f63915d9cedf1ed245ba6d8c78f98c19169
gur01 <vyachgurov@gmail.com>
2020-03-04 23:04:59 +0300

    split

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { Response, Request, NextFunction } from 'express';
 import morgan from 'morgan';
-import createError from 'http-errors';
+// import createError from 'http-errors';
 import dotenv from "dotenv";
 import router from './routes';
 dotenv.config();
@@ -14,8 +14,8 @@ app.use('/', router.test);
 app.use(morgan('dev'));
 
 // catch 404 and forward to error handler
-app.use(function(req: Request, res: Response, next:NextFunction) {
-    next(createError(404));
-  });
+// app.use(function(req: Request, res: Response, next:NextFunction) {
+//     // next(createError(404));
+//   });
 
 export default app;
